ba9114ca5d7e9699573ab6770c6b5726d0342e07
HanyAkoury <hany.akoury@gmail.com>
2018-11-30 15:25:57 +0100

    Reorganisation du WP

--- src/MonProject/Graph.cpp
+++ src/MonProject/Graph.cpp
@@ -1,30 +1,27 @@
 #include "Graph.h"
 #include <stdio.h>
-//#define TRACE_LINE(...) { ___debug( #__VA_ARGS__,  __VA_ARGS__,cerr); cerr<<"                    \033[100D";  }
-//#define TRACE_LINE(...) ;
 
 using namespace std;
 
-//visiblement, la méthode TRACE sert à afficher les messages qui effectuent le suivi de l'execution d'une application.
+//visiblement, la methode TRACE sert a afficher les messages qui effectuent le suivi de l'execution d'une application.
 
 //Constructeur de la classe Graphe
-Graph::Graph(string folder, string graph_file): folder(folder),graph_file(graph_file)
+Graph::Graph(string folder, string graph_file) : folder(folder),graph_file(graph_file)
 {
-    // lecture de la valeur de n et de m depuis le fichier attribute.txt
-    readNM();
+        // lecture de la valeur de n et de m depuis le fichier attribute.txt
+        readNM();
 
-    // initialisation des vecteurs du graphe
-    //boucle pour chaque noeud
-    for(int i=0; i<n; i++)
-    {
-        gT.push_back(vector<int>());
-        hasnode.push_back(false);
-        probT.push_back(vector<double>());
-        inDeg.push_back(0);
-    }
+        // initialisation des vecteurs du graphe
+        //boucle pour chaque noeud
+        for(int i=0; i<n; i++)
+        {
+                gT.push_back(vector<int>());
+                hasnode.push_back(false);
+                probT.push_back(vector<double>());
+                inDeg.push_back(0);
+        }
 
-    //lecture du graph
-    readGraph();
+        readGraph();
 }
 // methode permettant de choisir le modele de diffusion
 void Graph::setInfuModel(InfluModel modele)
@@ -33,101 +30,101 @@ void Graph::setInfuModel(InfluModel modele)
 }
 void Graph::Afficher()
 {
-    cout << "L'info voulue : " <<  m <<endl;
+        cout << "L'info voulue : " <<  m <<endl;
 }
 void Graph::readNM()
 {
-    // c_str() pour supprimer les esapces après la fin du mot
-    ifstream fichier((folder + "/attribute.txt").c_str());
+        // c_str() pour supprimer les esapces aprï¿½s la fin du mot
+        ifstream fichier((folder + "/attribute.txt").c_str());
 
-    // On verifie que le fichier a été trouvé
-     if(fichier)
-    {
-        string s;
-
-        // On lit le fichier ligne par ligne
-        while(fichier >> s)
+        // On verifie que le fichier a ï¿½tï¿½ trouvï¿½
+        if(fichier)
         {
-            //On verifie les deux premiers caractères de la ligne
-            if(s.substr(0,2)== "n=")
-            {
-                // atoi sert a convertir en Int
-                n=atoi(s.substr(2).c_str());
-                continue;
-            }
-            if(s.substr(0,2) == "m=")
-            {
-                m=atoi(s.substr(2).c_str());
-                continue;
-            }
-        }
-        fichier.close();
+                string s;
 
-    }
-    else
-    {
-        cout << "Impossible d'ouvrir le fichier " << endl;
-    }
+                // On lit le fichier ligne par ligne
+                while(fichier >> s)
+                {
+                        //On verifie les deux premiers caractï¿½res de la ligne
+                        if(s.substr(0,2)== "n=")
+                        {
+                                // atoi sert a convertir en Int
+                                n=atoi(s.substr(2).c_str());
+                                continue;
+                        }
+                        if(s.substr(0,2) == "m=")
+                        {
+                                m=atoi(s.substr(2).c_str());
+                                continue;
+                        }
+                }
+                fichier.close();
+
+        }
+        else
+        {
+                cout << "Impossible d'ouvrir le fichier " << endl;
+        }
 }
 
 void Graph::addEdge(int a, int b, double p)
 {
-    //push_back permet d'ajouter un element à la fin du vecteur.
-    probT[b].push_back(p);
-    gT[b].push_back(a);
-    inDeg[b]++;
+        //push_back permet d'ajouter un element ï¿½ la fin du vecteur.
+        probT[b].push_back(p);
+        gT[b].push_back(a);
+        inDeg[b]++;
 }
 
 void Graph::readGraph()
 {
-    FILE * fic = fopen((folder+"/"+graph_file).c_str(),"r");
+        FILE * fic = fopen((folder+"/"+graph_file).c_str(),"r");
 
-    if(fic)
-    {
-            //??
-            int readCnt=0;
+        if(fic)
+        {
+                //??
+                int readCnt=0;
 
-            // on parcourt le fichier ligne par ligne
-            // le fichier a exactement le meme nomnre de lignes que le graphe a d'arretes
-            for(int i=0; i<m; i++)
-            {
-                readCnt++;
-                // noeud de départ et neoud d'arrivée
-                int a,b;
-                // probabilité ???
-                double p;
-                int c=fscanf(fic,"%d%d%lf",&a,&b,&p);
-                // trois valeurs doivent etre récupérées pour chaque arrete, si ce n'est pas le cas,
-                // il y'a une erreur
-                if(c!= 3)
+                // on parcourt le fichier ligne par ligne
+                // le fichier a exactement le meme nomnre de lignes que le graphe a d'arretes
+                for(int i=0; i<m; i++)
                 {
-                    cout << "Le i: " << i << endl;
+                        readCnt++;
+                        // noeud de dï¿½part et neoud d'arrivï¿½e
+                        int a,b;
+                        // probabilitï¿½ ???
+                        double p;
+                        int c=fscanf(fic,"%d%d%lf",&a,&b,&p);
+                        // trois valeurs doivent etre rï¿½cupï¿½rï¿½es pour chaque arrete, si ce n'est pas le cas,
+                        // il y'a une erreur
+                        if(c!= 3)
+                        {
+                                cout << "Le i: " << i << endl;
+                        }
+
+                        // le numï¿½ro du noeud ne peut pas etre supï¿½rieur au nombre total de noeuds
+                        if(a>=n || b>=n)
+                        {
+                                cout << "erreur" <<endl;
+
+                        }
+                        cout << i << endl;
+                        //TRACE_LINE(a, b);
+                        //mettre le noeud de dï¿½part en existant
+                        hasnode[a]=true;
+                        //mettre le noeud d'arrivï¿½e en existant
+                        hasnode[b]=true;
+
+                        // ajout de l'arrete du noeud a vers le b avec la probabilitï¿½ p
+                        addEdge(a,b,p);
+
                 }
 
-                // le numéro du noeud ne peut pas etre supérieur au nombre total de noeuds
-                if(a>=n || b>=n)
-                {
-                    cout << "erreur" <<endl;
+                fclose(fic);
+        }
+        else
+        {
+                cout << "NOOOOOOOO"<< endl;
 
-                }
-                 cout << i << endl;
-                 //TRACE_LINE(a, b);
-                 //mettre le noeud de départ en existant
-                 hasnode[a]=true;
-                 //mettre le noeud d'arrivée en existant
-                 hasnode[b]=true;
-
-                 // ajout de l'arrete du noeud a vers le b avec la probabilité p
-                  addEdge(a,b,p);
-
-            }
-
-            fclose(fic);
-    }
-    else
-    {
-         cout << "NOOOOOOOO"<< endl;
-
-    }
+        }
 
 }
